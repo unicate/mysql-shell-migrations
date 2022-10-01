005662ea29088333e89e7347d56ed5f363a7cace
unicate <raoulschmid@bluewin.ch>
2022-10-01 16:27:51 +0200

    Updated sample migrations.

---
+++ sql/migrations/008_load_region.sql
@@ -0,0 +1,6 @@
+/*Data for the table regions */
+
+INSERT INTO regions(region_id,region_name) VALUES (1,'Europe');
+INSERT INTO regions(region_id,region_name) VALUES (2,'Americas');
+INSERT INTO regions(region_id,region_name) VALUES (3,'Asia');
+INSERT INTO regions(region_id,region_name) VALUES (4,'Middle East and Africa');
---
+++ sql/migrations/012_load_department.sql
@@ -0,0 +1,14 @@
+/*Data for the table departments */
+
+INSERT INTO departments(department_id,department_name,location_id) VALUES (1,'Administration',1700);
+INSERT INTO departments(department_id,department_name,location_id) VALUES (2,'Marketing',1800);
+INSERT INTO departments(department_id,department_name,location_id) VALUES (3,'Purchasing',1700);
+INSERT INTO departments(department_id,department_name,location_id) VALUES (4,'Human Resources',2400);
+INSERT INTO departments(department_id,department_name,location_id) VALUES (5,'Shipping',1500);
+INSERT INTO departments(department_id,department_name,location_id) VALUES (6,'IT',1400);
+INSERT INTO departments(department_id,department_name,location_id) VALUES (7,'Public Relations',2700);
+INSERT INTO departments(department_id,department_name,location_id) VALUES (8,'Sales',2500);
+INSERT INTO departments(department_id,department_name,location_id) VALUES (9,'Executive',1700);
+INSERT INTO departments(department_id,department_name,location_id) VALUES (10,'Finance',1700);
+INSERT INTO departments(department_id,department_name,location_id) VALUES (11,'Accounting',1700);
+
